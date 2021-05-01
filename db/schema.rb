4dde69c9dc6399c0e187a1035e9505ddfc11b1a5
reo66 <8409.reo@gmail.com>
2021-05-02 07:44:45 +0900

    user_mdel password_digestカラム追加ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_224119) do
+ActiveRecord::Schema.define(version: 2021_05_01_224313) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
