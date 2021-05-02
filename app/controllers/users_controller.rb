ad9dcf38a5683fe75f300837d84bd789046a6db4
reo66 <8409.reo@gmail.com>
2021-05-02 14:27:44 +0900

    user_params追加ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :department, :password, :password_confirmation)
+    end
+end
 
 end
