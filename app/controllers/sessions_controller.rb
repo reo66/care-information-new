62a5c95bb077066324ce63f52514416116e40c47
reo66 <8409.reo@gmail.com>
2021-05-02 18:20:36 +0900

    ログインok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = 'ログインに成功しました'
+      redirect_to user
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
 end
