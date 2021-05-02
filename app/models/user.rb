86c99c20fe62f472739d2bb05be8042e8e6c1d4b
reo66 <8409.reo@gmail.com>
2021-05-02 14:25:40 +0900

    user登録フォームok

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :department, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_224313) do
+ActiveRecord::Schema.define(version: 2021_05_02_051106) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "department"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
