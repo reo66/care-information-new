9de21214a49454a8cfc5541bb67b164efa74609c
reo66 <8409.reo@gmail.com>
2021-05-03 15:09:07 +0900

    remember_digestカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_051106) do
+ActiveRecord::Schema.define(version: 2021_05_03_060815) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_051106) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "department"
+    t.string "remember_digest"
   end
 
 end
