f0a30bf4a1dd7d961fd7560f68c63b1e52824a46
reo66 <8409.reo@gmail.com>
2021-05-04 10:49:12 +0900

    passwordをallow_nil.okok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :department, presence: true
 
   # 渡された文字列のハッシュ値を返します。
