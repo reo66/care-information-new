d640052758c237e7c71735dd3e3f7404b4d6396c
reo66 <8409.reo@gmail.com>
2021-05-04 11:08:33 +0900

    dimin追加ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_060815) do
+ActiveRecord::Schema.define(version: 2021_05_04_020642) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_060815) do
     t.string "password_digest"
     t.string "department"
     t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
