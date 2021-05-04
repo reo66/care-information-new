578b5ae7f6376eca94665149ba6b26c2ad9dee54
reo66 <8409.reo@gmail.com>
2021-05-04 14:57:48 +0900

    controller,routes okok

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
+  resources :care_users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
