5cfb47f5b126eaaf803a46a53d441c9ccf467208
reo66 <8409.reo@gmail.com>
2021-05-04 11:41:29 +0900

    修正ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 # coding: utf-8
 
 User.create!(name: "加藤玲央",
+  department: "本店",
   password: "password",
   password_confirmation: "password",
-  admin: true)
+  admin:true
+  )
 
-20.times do |n|
-name  = Faker::Name.name
-password = "password"
-User.create!(name: name,
-    password: password,
-    password_confirmation: password)
-end
+# # 20.times do |n|
+# # name  = Faker::Name.name
+# # password = "password"
+# # User.create!(name: name,
+# #     password: password,
+# #     password_confirmation: password)
+# end
