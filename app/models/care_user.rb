500c99e9474608c509219eb83a10d8d6d2ed8990
reo66 <8409.reo@gmail.com>
2021-05-12 08:19:25 +0900

    MiniMagick 準備ok

--- app/models/care_user.rb
+++ app/models/care_user.rb
@@ -6,6 +6,7 @@ class CareUser < ApplicationRecord
 
 # 利用者編集でupdateした時にストロングでカラムを配列にしているので、[""]となるのを防ぐ
 # https://www.tom08.net/entry/2016/09/16/175901
+# gsubメソッド⇨https://uxmilk.jp/23987 https://www.sejuku.net/blog/14685 https://www.sejuku.net/blog/60033
   before_save do
     self.use_day.gsub!(/[\[\]\"]/, "") if attribute_present?("use_day")
   end
