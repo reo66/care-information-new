359466f2f59c9b9248d8ef36efe3190e0fa6948a
reo66 <8409.reo@gmail.com>
2021-07-27 10:34:33 +0900

    ヘッダー英語表記ok

--- app/controllers/care_users_controller.rb
+++ app/controllers/care_users_controller.rb
@@ -49,7 +49,7 @@ class CareUsersController < ApplicationController
 
   
   def edit_index 
-    @care_users = @q.result(distinct: true).order("name DESC").page(params[:page])
+    @care_users = @q.result.page(params[:page])
     if @care_user.update(care_user_two_params)
       @count =  Intermediate.where(user_id: current_user.id, confirmation: false, indication: "更新")
     end
