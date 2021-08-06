0319c6d2caf42d85e933819128b48ff3e4f4a56c
reo66 <8409.reo@gmail.com>
2021-08-06 23:14:54 +0900

    gimei追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,147 +14,15 @@ password: "password",
 password_confirmation: "password",)
 end
 
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
+
+
+20.times do
+  gimei = Gimei.new
+
+CareUser.create!(
+name: gimei.name.kanji,
 department: "本店",
 grade: "未就学"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "年少"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "年中"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "年長"
-
-)
-end
-
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "小１"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "小2"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "本店",
-grade: "小3"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "小4"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "小5"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "小6"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "中１"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "中2"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "中3"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "高１"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "高２"
-
-)
-end
-
-2.times do |n|
-name  = Faker::Name.name
-CareUser.create!(name: name,
-department: "上高畑",
-grade: "高3"
 
 )
 end
