de995f5f45151342e5b6aa5db1a208cbabe1bec2
ryota-1231 <rrrrr.1231@gmail>
2021-08-22 10:57:16 +0900

    seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,20 +14,22 @@ password: "password",
 password_confirmation: "password",)
 end
 
+10.times do|n|
+  name  = Faker::Name.name
+CareUser.create!(
+name: name,
+department: "本店",
+grade: "未就学",
 
-
-# 10.times do|n|
-#   name  = Faker::Name.name
-# CareUser.create!(
-# name: name,
-# department: "本店",
-# grade: "未就学",
-# intermediate_attributes: [
-
-#   user_id: User.all,
-#   care_user_id: care_user.id,
-#   indication: 0
-#     ] 
-#   )
+)
 end
 
+CareUser.all.each do |care_user|
+  User.all.each do |user|
+    Intermediate.create!(
+      user_id: user.id,
+      care_user_id: care_user.id,
+      indication: 0
+    )
+  end
+end
