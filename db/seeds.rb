dca8a196dc6299cf3e47c289d990c9d252eceb1e
reo66 <8409.reo@gmail.com>
2021-08-22 21:39:31 +0900

    ボタン操作機能完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,25 @@ end
 
 
 
-# 10.times do|n|
-#   name  = Faker::Name.name
-# CareUser.create!(
-# name: name,
-# department: "本店",
-# grade: "未就学",
-# intermediate_attributes: [
 
-#   user_id: User.all,
-#   care_user_id: care_user.id,
-#   indication: 0
-#     ] 
-#   )
+
+30.times do |n|
+  name  = Faker::Name.name
+  CareUser.create!(
+    name: name,
+    department: "本店",
+    grade: "未就学",
+  )
 end
+
+
+CareUser.all.each do |care_user|
+  User.all.each do |user|
+    Intermediate.create!(
+      user_id: user.id,
+      care_user_id: care_user.id,
+      indication: 0
+    )
+  end
+end 
 
